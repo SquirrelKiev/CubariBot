2360538701f8b71e398325e3fb9a38ebcaebcd0e
SquirrelKiev <laidbackenon@outlook.com>
2023-08-28 23:25:59 +0100

    push test

--- interaction/Config.ts
+++ interaction/Config.ts
@@ -16,7 +16,7 @@ export const config = {
 
   // styling
   defaultButtonStyle: ButtonStyle.SECONDARY as ButtonStyle.PRIMARY | ButtonStyle.SECONDARY | ButtonStyle.SUCCESS | ButtonStyle.DESTRUCTIVE,
-  embedColor: 0xFFFFFF,
+  embedColor: 0xFFC0CB,
   invalidLinkError: "Unsupported/invalid URL. Please make sure you're using a link that is supported by Cubari.",
   
   // db
